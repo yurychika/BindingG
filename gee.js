48672ffde029232915192964e9a90b6d1932de0a
qi chen <yurychika@gmail.com>
2013-08-19 23:27:22 +0800

    first working version

--- gee.js
+++ gee.js
@@ -1,7 +1,7 @@
 var gee = {
 	viewModelList: [],
 	
-	observable: function(){
+	observable: function(initialValue){
 		var func = function(){
 			if(arguments.length == 0){
 				return this.__value;
@@ -9,8 +9,9 @@ var gee = {
 				this.__value = arguments[0];
 			}
 		};
-		fun.__value = {};
+		func.__value = initialValue;
 		func.__observers = [];
+		return func;
 	},
 	
 	bind: function(viewModel, documentRoot){
@@ -25,23 +26,58 @@ var gee = {
 		
 		for(var i = 0; i < tags.length; i++){
 			var t = tags[i];
-			var dbAttr = t.getAttribute('data-bind');
-			if(dbAttr){
-				dbAttr = '{' + dbAttr + '}';
-				var as = JSON.parse(dbAttr);	//attributes
+			var dataBindAttr = t.getAttribute('data-bind');
+			if(dataBindAttr){
+				// var as = JSON.parse(dbAttr);	//attributes
+				var as = this._parseAttribute(dataBindAttr);
 				for(var i in as){
-					console.log(i, as[i]);
-					if(vm.hasOwnProperty(as[i])){
-						vm[as[i]].__observers.push({node: t, type: i});		//add observers to the specific function
+					console.log(viewModel)
+					console.log(as[i])
+					if(viewModel.hasOwnProperty(as[i])){
+						console.log('vm is' + viewModel);
+						viewModel[as[i]].__observers.push({node: t, type: i});		//add observers to the specific function
 						
 						if(i == 'text'){
-							var observable = vm[as[i]];
+							console.log(123)
+							var observable = viewModel[as[i]];
+							console.log(t);
 							t.innerHTML = observable.apply(observable, []);
 						}
 					}
 				}		//end of for
 			}		//end of if
 		}		//end of for
+	},
+	
+	_parseAttribute: function(str){
+		var attrs = str.split(',');
+		var obj = {};
+		for(var i = 0; i < attrs.length; i++){
+			var attrPair = attrs[i].split(':');
+			var key = attrPair[0];
+			var value = attrPair[1];
+			
+			// if(value.indexOf('"') < 0 && value.indexOf("'") < 0){
+				// value = '"' + value + "'";
+			// }
+			obj[key] = value;		
+		}
+		
+		return obj;
 	}
+	
+	
+	
+	
+	
+	
+
+
+
+
+
+
+
+	
 	
 }
