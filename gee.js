cf988e41915fbb9f1b67f8bd6ddd87a581a83879
Leslie <yurychika@gmail.com>
2013-08-19 13:52:52 +0800

    still issue in JSON.parse
    How to parse the attribute in the tag to JSON?

--- gee.js
+++ gee.js
@@ -14,14 +14,34 @@ var gee = {
 	},
 	
 	bind: function(viewModel, documentRoot){
+		var d = documentRoot? documentRoot : document;
 		viewModel.__documentContext = documentRoot;
 		this.viewModelList.push(viewModel);
+		this.parse(viewModel, d);
 	},
 	
-	parse: function(){
+	parse: function(viewModel, documentRoot){
+		var tags = documentRoot.getElementsByTagName('*');
 		
+		for(var i = 0; i < tags.length; i++){
+			var t = tags[i];
+			var dbAttr = t.getAttribute('data-bind');
+			if(dbAttr){
+				dbAttr = '{' + dbAttr + '}';
+				var as = JSON.parse(dbAttr);	//attributes
+				for(var i in as){
+					console.log(i, as[i]);
+					if(vm.hasOwnProperty(as[i])){
+						vm[as[i]].__observers.push({node: t, type: i});		//add observers to the specific function
+						
+						if(i == 'text'){
+							var observable = vm[as[i]];
+							t.innerHTML = observable.apply(observable, []);
+						}
+					}
+				}		//end of for
+			}		//end of if
+		}		//end of for
 	}
-	
-	
 	
 }
